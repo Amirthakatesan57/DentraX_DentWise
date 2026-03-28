7e1e60baedf60d967a68630c2adad6d7e7d667f6
Amirthakatesan M <119378816+Amirthakatesan57@users.noreply.github.com>
2026-03-28 08:25:28 +0530

    Add dynamic export to page component

--- src/app/pro/page.tsx
+++ src/app/pro/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Navbar from "@/components/Navbar";
 import { PricingTable } from "@clerk/nextjs";
 import { auth, currentUser } from "@clerk/nextjs/server";
